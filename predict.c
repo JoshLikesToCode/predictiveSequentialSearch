f307508318f58d0a5775449a6b3dd8b24cc8eda7
Joshua Stone <39971298+JoshLikesToCode@users.noreply.github.com>
2021-02-18 16:34:55 -0800

    Update predict.c

--- predict.c
+++ predict.c
@@ -97,49 +97,49 @@ void find_item(int* arr, int size, int key)
 							return;
 						}
 					}
-               		}
-                	else if(last_value < key)
-                	{
-				i = last_index+1;
+               			}
+				else if(last_value < key)
+				{
+					i = last_index+1;
 
-				if(i > size)
-				{
-					i -= 1;
-				}
-				int z = 0;
-				for(; i < size; i++)
-				{
-					z++;
-					if(arr[i] == key)
+					if(i > size)
 					{
-						last_index = i;
-						last_value = key;
-						printf("3 - The key of %d was found in %d steps.\n", key, z);
-						predict_steps += i;
-						predicted_searches++;
-						return;
+						i -= 1;
 					}
-				}
-				for(int j = 0; j < size; j++)
-				{
-					if(arr[j] == key)
+					int z = 0;
+					for(; i < size; i++)
 					{
-						last_index = j;
-						last_value = key;
-						printf("4 - The key of %d was found in %d steps.\n", key, z+j);
-						predict_steps += (z+j);
-						return;
+						z++;
+						if(arr[i] == key)
+						{
+							last_index = i;
+							last_value = key;
+							printf("3 - The key of %d was found in %d steps.\n", key, z);
+							predict_steps += i;
+							predicted_searches++;
+							return;
+						}
 					}
-				}
+					for(int j = 0; j < size; j++)
+					{
+						if(arr[j] == key)
+						{
+							last_index = j;
+							last_value = key;
+							printf("4 - The key of %d was found in %d steps.\n", key, z+j);
+							predict_steps += (z+j);
+							return;
+						}
+					}
 
-			}
-			else if(last_value == key)
-			{
-				i = last_index;
-				printf("The key of %d was found in 0 steps.\n", key);
-				predicted_searches++;
-				return;
-			}
+				}
+				else if(last_value == key)
+				{
+					i = last_index;
+					printf("The key of %d was found in 0 steps.\n", key);
+					predicted_searches++;
+					return;
+				}
 
 			printf("The key you were looking for doesn't exist.\n");
 			predict_steps += size;
@@ -158,8 +158,9 @@ void find_item(int* arr, int size, int key)
 					regular_steps += i;
 					regular_searches++;
 					return;
-					}
 				}
+			}
+
 			printf("The key wasn't found\n");
 			regular_steps += i;
 			regular_searches++;
